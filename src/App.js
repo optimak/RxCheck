404be74872745d0d18c9ba00df4ec140c9305461
Chi Aghaizu <chiaghaizu@gmail.com>
2024-03-30 09:22:26 -0400

    set up fonts, favicon, title, partials and installed dependencied (axios, react-router-dom, sass and validator)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      The quick brown fox jumps over the lazy dog
+      <h1>The quick brown fox jumps over the lazy dog</h1>
+      <h2>The quick brown fox jumps over the lazy dog</h2>
     </div>
   );
 }
