aae0ccaa77f5253849d159dc6527e7b45ef7d5a1
Chi Aghaizu <chiaghaizu@gmail.com>
2024-04-24 14:47:03 -0400

    resolve merge conflicts in about page

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -16,16 +16,11 @@ function AboutPage({ userMeds, deleteUserMed }) {
 
 
 
-<<<<<<< HEAD
-
-   
-=======
     // const savedSection = useRef(null);
 
     // const scrollTo = () => {
     //     window.scrollTo({ top: savedSection.current.offsetTop, behavior: 'smooth', });
     // }
->>>>>>> develop
 
 
     const getProfile = async () => {
@@ -37,10 +32,6 @@ function AboutPage({ userMeds, deleteUserMed }) {
             });
 
             // setProfile(response.data);
-<<<<<<< HEAD
-            
-=======
->>>>>>> develop
             sessionStorage.setItem('profileId', JSON.stringify(response.data.id));
 
 
